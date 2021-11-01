67f52f78946ebe5428e37dffa3832e065c1f8014
Viacheslav Katsuba <v.katsuba.dev@gmail.com>
2021-11-01 17:05:08 +0200

    Update test/{{ repo-name }}_SUITE.erl
    Co-authored-by: Brujo Benavides <elbrujohalcon@gmail.com>

--- test/{{ repo-name }}_SUITE.erl
+++ test/{{ repo-name }}_SUITE.erl
@@ -8,19 +8,21 @@
 -export([all/0, groups/0, init_per_testcase/2, end_per_testcase/2]).
 -export([success/1]).
 
--spec all() -> list().
+-spec all() -> [ct_suite:ct_test_def(), ...].
 all() ->
     [success].
 
--spec groups() -> list().
+-spec groups() -> [ct_suite:ct_group_def(), ...].
 groups() ->
     [].
 
--spec init_per_testcase(term(), list()) -> list().
+-spec init_per_testcase(ct_suite:ct_testname(), ct_suite:ct_config()) ->
+                           ct_suite:ct_config() | {fail, term()} | {skip, term()}.
 init_per_testcase(_Name, Config) ->
     Config.
 
--spec end_per_testcase(term(), list()) -> ok.
+-spec end_per_testcase(ct_suite:ct_testname(), ct_suite:ct_config()) ->
+                          term() | {fail, term()} | {save_config, ct_suite:ct_config()}.
 end_per_testcase(_Name, _Config) ->
     ok.
 
